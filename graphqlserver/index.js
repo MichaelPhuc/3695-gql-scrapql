1c1182820826609ff0e6a8f39ad431f6900e4228
MichaelPhuc <49359137+MichaelPhuc@users.noreply.github.com>
2020-10-01 11:39:50 -0700

    Finished assignment 1

--- graphqlserver/index.js
+++ graphqlserver/index.js
@@ -3,16 +3,58 @@ import apolloServer from 'apollo-server'
 const { ApolloServer, gql } = apolloServer
 
 const typeDefs = gql`
+    type Note {
+        title: String
+        content: String
+        dateCreated: String
+    }
+    
+    type Reminder {
+        title: String
+        content: String
+        reminderTime: String
+    }
+
     type Query {
-    hello: String
-}
+        notes: [Note]
+        reminders: [Reminder]
+    }
+
 `
+const notes =[
+    {
+        title: 'Avocado Egg Toast Recipe',
+        content: '4 slices of bread, 4 eggs, 1 avocado',
+        dateCreated: "2020-10-01"
+    },
+    {
+        title: 'How to become the best at GraphQl',
+        content: 'Learn',
+        dateCreated: "2020-10-01"
+    }
+]
+
+const reminders = [
+    {
+        title: 'Do Homework',
+        content: 'ACIT 3695 Assignment 1',
+        reminderTime: "2020-10-01 10:00"
+    },
+    {
+        title: 'Workout',
+        content: 'Leg day!',
+        reminderTime: "2020-10-01 10:00"
+    }
+]
 
 const resolvers = {
     Query: {
-        hello: () => "hello world graphql version"
-    }
+        notes: () => notes,
+        reminders: () => reminders
+    },
 }
+
+
 
 const server = new ApolloServer({typeDefs, resolvers})
 server.listen({port: 4000}).then(({url}) => console.log(`server running at ${url}`))
